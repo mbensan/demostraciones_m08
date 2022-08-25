b5b24449f4ae8bd736673ced1c6c63b5ab6e53bc
Matias Bensan A <mbensan@gmail.com>
2022-08-25 12:23:21 -0400

    Se resuleve ejercicio random words

--- ej_sesiones/server.js
+++ ej_sesiones/server.js
@@ -1,10 +1,20 @@
-const { query } = require('express');
 const express = require('express');
 const session = require('express-session');
+const nunjucks = require('nunjucks')
+const path = require('path')
 
 const app = express()
 
+// se configura uso de sesiones
 app.use(session({secret: 'hmit'}))
+
+// se configura nunjucks
+nunjucks.configure(path.resolve(__dirname, "templates"), {
+  express: app,
+  autoscape: true,
+  noCache: true,
+  watch: true,
+});
 
 app.get('/', (req, res) => {
   const letras = req.session.letras || ''
@@ -14,7 +24,6 @@ app.get('/', (req, res) => {
 app.get('/add', (req, res) => {
   // primero me traigo la variable de session
   let letras = req.session.letras || ''
-  console.log(letras);
   // modifico la variable
   const letra = req.query.letra
   letras += letra
@@ -22,6 +31,28 @@ app.get('/add', (req, res) => {
   req.session.letras = letras
 
   res.send(`Letras: ${letras}`)
+})
+
+app.get('/random', (req, res) => {
+  const palabra = Math.random().toString(36).substr(2,24);
+  
+  if (req.session.intentos == undefined) {
+    req.session.intentos = 0
+  }
+
+  req.session.intentos++
+
+  const datos = {
+    intentos: req.session.intentos,
+    palabra
+  }
+
+  res.render('random.html', datos)
+})
+
+app.get('/random/reset', (req, res) => {
+  req.session.intentos = 0
+  res.redirect('/random')
 })
 
 app.listen(3000, () => {
